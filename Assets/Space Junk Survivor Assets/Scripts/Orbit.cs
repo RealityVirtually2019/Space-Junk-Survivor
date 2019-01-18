ac002799dac9205536bd994bdae94de855fb7930
Trevor Morrisey <trmorrisey@gmail.com>
2019-01-18 14:50:47 -0500

    Implemented very basic orbit mechanic

---
+++ Assets/Space Junk Survivor Assets/Scripts/Orbit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public Vector3 orbitVector;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, orbitVector, speed * Time.deltaTime);
+    }
+
+}
