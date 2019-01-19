67bb747c2d902a9b700ab3c08d8206719c45bd3e
Trevor Morrisey <trmorrisey@gmail.com>
2019-01-18 22:46:44 -0500

    Prototyped shattering

--- Assets/Space Junk Survivor Assets/Scripts/Interactable.cs
+++ Assets/Space Junk Survivor Assets/Scripts/Interactable.cs
@@ -6,12 +6,32 @@ using UnityEngine;
 public class Interactable : MonoBehaviour
 {
     public Hand m_ActiveHand = null;
+    public bool canShatter;
+    public Transform intact;
+    public List<Transform> pieces;
 
     private void OnTriggerEnter(Collider other)
     {
         if (m_ActiveHand && other.gameObject.CompareTag("Wieldable"))
         {
-            Destroy(other.gameObject);
+            other.gameObject.GetComponent<Interactable>().Hit();
+        }
+    }
+
+    public void Hit()
+    {
+        if (canShatter)
+        {
+            intact.gameObject.SetActive(false);
+            foreach (Transform t in pieces)
+            {
+                t.gameObject.SetActive(true);
+                t.GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+            }
+        }
+        else
+        {
+            gameObject.SetActive(false);
         }
     }
 }
